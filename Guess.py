915a0b2386d08b98ded255a4dbe7fc4471a6f691
jasimrazi <jasimrazi17@gmail.com>
2022-08-16 22:58:10 +0530

    Guess the number

---
+++ Guess.py
@@ -0,0 +1,43 @@
+from operator import truediv
+import random
+
+range=input("Please type a range: ")
+if range.isdigit():
+    range=int(range)
+    if range<=0:
+        print('Please type a number more than 0 and try again')
+        input()
+else:
+    print("Please a type a 'number'")
+    input()
+
+random_number=random.randint(0,range)
+# random_number=int(random_number)
+total = 0
+
+while True:
+    guess=input("Guess the number: ")
+    if guess.isdigit():
+        guess=int(guess)
+        total+=1
+    else:
+        print("[Please a type a 'number' this time]")
+        continue
+
+    if guess == random_number:
+        print("You got it right!")
+        print("\nYou took",total,"guesses in total.")
+        input()
+        break
+    elif guess < random_number:
+        print("Your guess is below the number")
+    else:
+        print("Your guess is above the number")
+        
+
+# print("\nYou took",total,"guesses in total.")
+
+
+
+
+
