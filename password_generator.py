3f3aa07aef1a91a0a81e35ebb0dd7404bbe8c8f8
jasimrazi <jasimrazi17@gmail.com>
2022-08-22 22:46:15 +0530

    Password Generator

---
+++ password_generator.py
@@ -0,0 +1,21 @@
+import random
+import string
+
+print("Welcome to Password Generator\n")
+
+length = int(input("Enter the lenth of the password: "))
+
+letters = string.ascii_letters
+numbers = string.digits
+specials = "!@#$%&"
+
+all = letters + numbers + specials
+
+temp_password = random.sample(all,length)
+password = "".join(temp_password)
+
+print("Your password is:",password)
+input()
+
+
+
