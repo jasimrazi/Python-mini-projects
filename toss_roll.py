85b1a1f7f1e25022ecee223827f4ab76a7171dec
jasimrazi <jasimrazi17@gmail.com>
2022-08-23 00:08:36 +0530

    Add loop structure

--- toss_roll.py
+++ toss_roll.py
@@ -1,19 +1,28 @@
 import random
 
-print("[Please choose an option]")
-answer = input("Do you want to 'roll a die' or 'toss a coin'? (A/B): ").lower() 
+print("[Please type Q to quit]")
 
-if (answer=='a' or answer=='roll a die'):
-    r1=random.randrange(1,7)
-    print("The die landed on",r1)
+while True:
 
-elif (answer=='b' or answer=='toss a coin'):
-    list = ['head','tail']
-    r2=random.choice(list)
-    print("The coin landed on",r2)
+    answer = input(
+        "Do you want to 'roll a die' or 'toss a coin'? (A/B): ").lower()
 
-else:
-    quit()
+    if (answer == 'a' or answer == 'roll a die'):
+        r1 = random.randrange(1, 7)
+        print("The die landed on", r1)
+        continue
+
+    elif (answer == 'b' or answer == 'toss a coin'):
+        list = ['head', 'tail']
+        r2 = random.choice(list)
+        print("The coin landed on", r2)
+        continue
+
+    elif (answer == "q"):
+        break
+
+    else:
+        print("Invalid Input")
 
 
-input()
+quit()
